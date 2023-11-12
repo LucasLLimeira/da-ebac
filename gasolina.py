c47424915d237ad7c466dd5b8da8a1998ad14679
LucasLLimeira <lucasdllimeira@hotmail.com>
2023-11-12 18:49:21 +0000

    adicionado 3 arquivos sobre preço da gasolina

---
+++ gasolina.py
@@ -0,0 +1,13 @@
+import seaborn as sns
+import pandas as pd
+import csv
+
+gasolina_df = pd.read_csv('gasolina.csv')
+
+with sns.axes_style('whitegrid'):
+
+  grafico = sns.lineplot(data=gasolina_df, x='dia', y='venda', palette ='pastel')
+  grafico.set(title='Preço da gasolina por dia', xlabel='Dias', ylabel='Preço (R$)');
+
+grafico.get_figure().savefig('gasolina.png')
+
